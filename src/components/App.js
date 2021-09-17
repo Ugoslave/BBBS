1cec0d7e160aaf62919aeec2ffeff9fbc554ceb7
Alexey Peresypkin <ugo200@yandex.ru>
2021-09-17 08:52:58 +0500

    blocks Webpack

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import AboutProject from "./AboutProject";
+import React from "react";
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <AboutProject />
+      </div>
+    </>
+  );
+}
+
+export default App;
